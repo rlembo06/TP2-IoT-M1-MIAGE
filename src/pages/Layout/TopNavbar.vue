43ee33b8ffe4aea65d17020d1182620287df24ef
Romain Lembo <romainlembo06@gmail.com>
2019-02-04 08:49:37 +0100

    init

---
+++ src/pages/Layout/TopNavbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <md-toolbar md-elevation="0" class="md-transparent">
+    <div class="md-toolbar-row">
+      <div class="md-toolbar-section-start">
+        <h3 class="md-title">{{$route.name}}</h3>
+      </div>
+    </div>
+  </md-toolbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="css">
+</style>
