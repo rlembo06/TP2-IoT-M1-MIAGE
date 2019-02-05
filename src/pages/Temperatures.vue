6510024a869f52a42b44c5e3858b5b6f0380e9ac
Romain Lembo <romainlembo06@gmail.com>
2019-02-05 17:29:13 +0100

    chart temperatures displayed

--- src/pages/Temperatures.vue
+++ src/pages/Temperatures.vue
@@ -1,23 +1,60 @@
 <template>
     <div> 
-        <h4>All :</h4>
-        <code>{{temperatures}}</code>
+        <div>
+            <code>{{temperatures}}</code>
+        </div>
+        <div>
+            <chart-card
+                :chart-data="temperatures.data"
+                :chart-type="'Line'"
+                data-background-color="blue">
+                <template slot="content">
+                    <h4 class="title">Températures</h4>
+                </template>
+
+                <template slot="footer">
+                    <div class="stats">
+                    <md-icon>access_time</md-icon>
+                    Dernière mise à jour : {{lastUpdate}}
+                    </div>
+                </template>
+            </chart-card>
+        </div>
     </div>
 </template>
 
 <script>
 import firestore from "../api/firestore_rest.js";
+import { ChartCard } from "@/components";
+import moment from "moment"
 
 export default {
     data: () => ({
-        temperatures: []
+        temperatures: {
+            data: {
+                labels: [],
+                series: [[]]
+            },
+        },
+        lastUpdate: null,
     }),
+    components: {
+        ChartCard,
+    },
     mounted() {
-        this.getTemperaturesDetails()
+        this.getTemperaturesChart();
+        console.log("temperatures: ", this.temperatures)
     },
     methods: {
-        async getTemperaturesDetails() {
-            this.temperatures = await firestore.getTemperatures();
+        async getTemperaturesChart() {
+            const jsonObserver = await firestore.getTemperatures() || [];
+            const data = JSON.parse(JSON.stringify(jsonObserver)).documents
+            data.forEach(item => {
+                this.temperatures.data.labels.push(moment(item.createTime).format('h:mm:ss a'))
+                this.temperatures.data.series[0].push(item.fields.temperatureInCelsius.doubleValue)
+            });
+            this.lastUpdate = data[data.length - 1].createTime
+            this.lastUpdate = moment(this.lastUpdate).format('MMMM Do YYYY, h:mm:ss a')
         }
     }
 }
