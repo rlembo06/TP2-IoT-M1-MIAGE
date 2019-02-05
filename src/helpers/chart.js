b0009ccae7b49e3e569b3ff96639eeee68477688
Romain Lembo <romainlembo06@gmail.com>
2019-02-05 21:28:59 +0100

    sort by date

--- src/helpers/chart.js
+++ src/helpers/chart.js
@@ -1,6 +1,11 @@
 import moment from "moment";
 
 export default {
+  sortByDate(documents) {
+    return documents.sort(
+      (a, b) => new Date(a.createTime) - new Date(b.createTime)
+    );
+  },
   lastUpdateTemperatures(documents) {
     return moment(documents[documents.length - 1].createTime).format(
       "MMMM Do YYYY, h:mm"
@@ -11,6 +16,7 @@ export default {
     documents.length >= maxElements &&
       documents.slice(documents.length - maxElements);
 
+    this.sortByDate(documents);
     documents.forEach(doc => {
       result.push({
         date: moment(doc.createTime).format("h:mm:ss a"),
