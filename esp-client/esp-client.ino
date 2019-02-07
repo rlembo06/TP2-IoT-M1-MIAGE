81bdc89500959f70f8bc5eeee54cb35b394a631c
Victor Monsch <victor.monsch@gmail.com>
2019-02-07 17:05:30 +0100

    Polished esp setup, couldn't get httpclient getstring to work with google apis

--- esp-client/esp-client.ino
+++ esp-client/esp-client.ino
@@ -1,16 +1,22 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
+#define MILLI_FACTOR 1000
 #define MICRO_FACTOR 1000000
-#define SLEEP_TIME 20
-#define TIME_TO_UP 10
+#define SLEEP_TIME 60 * 2
+#define UP_TIME 60 * 1
 
-const int ledPin = 23;
-const int temperaturePin = 19;
+const int ledPin = 19;
+const int temperaturePin = 23;
 const int brightnessPin = A0;
 
 const int serial = 9600;
+
+OneWire oneWire(temperaturePin);
+DallasTemperature temperatureSensor(&oneWire);
 
 void ledOn() {
   digitalWrite(ledPin, HIGH);
@@ -28,7 +34,7 @@ void ledBlink(int numberOfBlinks) {
 void connectWifi() {
   const char * wifiSsid = "victor-mobile";
   const char * wifiPass = "parasite";
-  const int connectionTimeout = 180;
+  const int connectionTimeout = 30;
 
   WiFi.begin(wifiSsid, wifiPass);
   Serial.print("Connecting to WiFi...");
@@ -115,7 +121,7 @@ void createBrightnessEntry(double brightness) {
   char jsonMessageBuffer[300];
   root.prettyPrintTo(jsonMessageBuffer, sizeof(jsonMessageBuffer));
   Serial.print("Attempting to create a brightness document with a value of ");
-  Serial.print(temperature);
+  Serial.print(brightness);
   Serial.print(" from address ");
   Serial.print(WiFi.macAddress());
   Serial.print(". \n");
@@ -130,47 +136,88 @@ void createBrightnessEntry(double brightness) {
   client.end();
 }
 
-JsonObject getConfigurations() {
-  char * restUrl = "https://firestore.googleapis.com/v1/projects/tp2-iot-m1-miage/databases/(default)/documents/configurations/cB1CljpmA9o0lRsUvuQh"; // specific document containing our values
+JsonObject & getConfigurations() {
+  char * restUrl = "https://firestore.googleapis.com/v1/projects/tp2-iot-m1-miage/databases/(default)/documents/configurations/cB1CljpmA9o0lRsUvuQh";
+  //char * restUrl = "http://httpbin.org/ip";
+  //char * restUrl = "https://httpbin.org/json";
   HTTPClient client;
 
+  /*char * certificate = "-----BEGIN CERTIFICATE-----\n" \
+"MIIDujCCAqKgAwIBAgILBAAAAAABD4Ym5g0wDQYJKoZIhvcNAQEFBQAwTDEgMB4G\n" \
+"A1UECxMXR2xvYmFsU2lnbiBSb290IENBIC0gUjIxEzARBgNVBAoTCkdsb2JhbFNp\n" \
+"Z24xEzARBgNVBAMTCkdsb2JhbFNpZ24wHhcNMDYxMjE1MDgwMDAwWhcNMjExMjE1\n" \
+"MDgwMDAwWjBMMSAwHgYDVQQLExdHbG9iYWxTaWduIFJvb3QgQ0EgLSBSMjETMBEG\n" \
+"A1UEChMKR2xvYmFsU2lnbjETMBEGA1UEAxMKR2xvYmFsU2lnbjCCASIwDQYJKoZI\n" \
+"hvcNAQEBBQADggEPADCCAQoCggEBAKbPJA6+Lm8omUVCxKs+IVSbC9N/hHD6ErPL\n" \
+"v4dfxn+G07IwXNb9rfF73OX4YJYJkhD10FPe+3t+c4isUoh7SqbKSaZeqKeMWhG8\n" \
+"eoLrvozps6yWJQeXSpkqBy+0Hne/ig+1AnwblrjFuTosvNYSuetZfeLQBoZfXklq\n" \
+"tTleiDTsvHgMCJiEbKjNS7SgfQx5TfC4LcshytVsW33hoCmEofnTlEnLJGKRILzd\n" \
+"C9XZzPnqJworc5HGnRusyMvo4KD0L5CLTfuwNhv2GXqF4G3yYROIXJ/gkwpRl4pa\n" \
+"zq+r1feqCapgvdzZX99yqWATXgAByUr6P6TqBwMhAo6CygPCm48CAwEAAaOBnDCB\n" \
+"mTAOBgNVHQ8BAf8EBAMCAQYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUm+IH\n" \
+"V2ccHsBqBt5ZtJot39wZhi4wNgYDVR0fBC8wLTAroCmgJ4YlaHR0cDovL2NybC5n\n" \
+"bG9iYWxzaWduLm5ldC9yb290LXIyLmNybDAfBgNVHSMEGDAWgBSb4gdXZxwewGoG\n" \
+"3lm0mi3f3BmGLjANBgkqhkiG9w0BAQUFAAOCAQEAmYFThxxol4aR7OBKuEQLq4Gs\n" \
+"J0/WwbgcQ3izDJr86iw8bmEbTUsp9Z8FHSbBuOmDAGJFtqkIk7mpM0sYmsL4h4hO\n" \
+"291xNBrBVNpGP+DTKqttVCL1OmLNIG+6KYnX3ZHu01yiPqFbQfXf5WRDLenVOavS\n" \
+"ot+3i9DAgBkcRcAtjOj4LaR0VknFBbVPFd5uRHg5h6h+u/N5GJG79G+dwfCMNYxd\n" \
+"AfvDbbnvRG15RjF+Cv6pgsH/76tuIMRQyV+dTZsXjAzlAcmgQWpzU/qlULRuJQ/7\n" \
+"TBj0/VLZjmmx6BEP3ojY+x1J96relc8geMJgEtslQIxq/H5COEBkEveegeGTLg==\n" \
+"-----END CERTIFICATE-----\n";*/
+
   client.begin(restUrl);
-  //client.addHeader("Content-Type", "application/json");
-  //client.addHeader("Content-Length", "100"); // TODO check if needed
 
   int httpStatus = client.GET();
 
-  client.end(); // TODO check if this is too soon
-
   if (httpStatus == 200) {
+    String httpContent = client.getString();
+    Serial.print(httpContent);
+    
     StaticJsonBuffer<300> jsonBuffer;
-    JsonObject & configurations = jsonBuffer.parseObject(client.getString());
+    JsonObject & configurations = jsonBuffer.parseObject(httpContent);
+    
+    Serial.print("Configurations are now set to a temperature switch of ");
+    Serial.print(configurations["fields"]["temperatureSwitch"].as<double>());
+    Serial.print(" and a brightness switch of ");
+    Serial.print(configurations["fields"]["brightnessSwitch"].as<double>());
+    Serial.print(". \n");
+
+    ledBlink(10);
 
     return configurations;
-    // Serial.print("Configurations are now set to a temperature switch of ");
-    // Serial.print();
-    // Serial.print(" and a brightness switch of ");
-    // Serial.print();
-    // Serial.print(". \n");
-
-    // ledBlink(10);
   }
 
-  return null;
+  client.end();
+}
+
+void checkConfigurations(double temperatureValue, double brightnessValue) {
+  JsonObject & configurations = getConfigurations();
+
+  if (temperatureValue > configurations["fields"]["temperatureSwitch"] && brightnessValue > configurations["fields"]["brightnessValue"]) {
+    Serial.println("Configuration requirements are met : both our temperature and brightness values are high enough.");
+    ledOn();
+  }
+  else {
+    Serial.println("Configuration requirements are not met : at least one of our temperature and brightness values wasn't high enough.");
+  }
+}
+
+void stayAwake() {
+  Serial.print("Staying awake for ");
+  Serial.print(UP_TIME);
+  Serial.print("s. \n");
+    
+  delay(UP_TIME * MILLI_FACTOR);
 }
 
 void hibernate() {
-  Serial.println("Hibernating now.");
-
   WiFi.disconnect();
-  //esp_sleep_enable_timer_wakeup(SLEEP_TIME * MICRO_FACTOR);
+
+  Serial.print("Hibernating now for ");
+  Serial.print(SLEEP_TIME);
+  Serial.print("s. \n");
 
   esp_deep_sleep(SLEEP_TIME * MICRO_FACTOR);
-  Serial.print("Should be asleep now, here goes hoping you don't see this...");
-}
-
-void stayAwake(int timeToStayAwake) {
-  delay(timeToStayAwake);
 }
 
 void setup() {
@@ -182,21 +229,19 @@ void setup() {
 
   if (WiFi.status() == WL_CONNECTED) {
     ledBlink(2);
+    
+    double brightnessValue = analogRead(brightnessPin);
+    
+    temperatureSensor.requestTemperaturesByIndex(0); 
+    double temperatureValue = temperatureSensor.getTempCByIndex(0); 
 
-    int brightnessValue = analogRead(brightnessPin);
-    //int temperatureValue = // TODO fetch temperature (copy code from previous works)
+    createTemperatureEntry(temperatureValue);
+    createBrightnessEntry(brightnessValue);
 
-    createTemperatureEntry(brightnessValue);
-    //createTemperatureEntry(temperatureValue);
+    //checkConfigurations(temperatureValue, brightnessValue);
 
-    JsonObject configurations = getConfigurations();
-
-    if (temperatureValue > configurations.temperatureSwitch && brightnessValue > configurations.brightnessValue) {
-      ledOn();
-    }
-
-    stayAwake(TIME_TO_UP);
-
+    stayAwake();
+    
     hibernate();
   }
 }
