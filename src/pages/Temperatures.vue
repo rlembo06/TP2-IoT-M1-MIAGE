0962b1cc4d9c1ee6c7a2ae05ff4e609604920bc4
Romain Lembo <romainlembo06@gmail.com>
2019-02-07 12:19:18 +0100

    add buttun refresh + limen

--- src/pages/Temperatures.vue
+++ src/pages/Temperatures.vue
@@ -1,5 +1,19 @@
 <template>
     <div>
+        <div class="md-layout">
+            <div class="md-layout-item">
+                <md-button class="md-raised md-success" @click="getTemperaturesChart()">
+                    <md-icon>cached</md-icon>
+                    <md-tooltip md-direction="top">Raffraîchir</md-tooltip>
+                </md-button>
+            </div>
+            <div class="md-layout-item">
+                <md-field>
+                    <label>Seuil C°</label>
+                    <md-input v-model="limen" type="number" min="-20" max="40"></md-input>
+                </md-field>
+            </div>
+        </div>
         <div class="small">
             <ve-line :data="chartData"></ve-line>
         </div>
@@ -46,6 +60,7 @@ export default {
         },
         lastUpdate: null,
         maxElements: 10,
+        limen: 10,
     }),
     components: {
         StatsCard
@@ -55,10 +70,11 @@ export default {
     },
     methods: {
         async getTemperaturesChart() {
+            console.log("!!")
             const { documents } = await firestore.getTemperatures();
             this.chartData.rows = chartHelpers.chartLineData("celsius", documents, this.maxElements);
             this.lastUpdate = chartHelpers.lastUpdateData(documents);
-        }
+        },
     }
 }
 </script>
